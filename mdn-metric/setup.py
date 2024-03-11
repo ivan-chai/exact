02700694eabee06539d6a4f9868a4d0b9c9cb0d8
Ivan Karpukhin <karpuhini@yandex.ru>
2024-03-11 11:14:51 +0300

    Revert setup.py

--- mdn-metric/setup.py
+++ mdn-metric/setup.py
@@ -11,21 +11,21 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         "catalyst==21.9",
-        "faiss-cpu",  # Need for MAP@R metric computation.
-        "jpeg4py",
-        "mxnet",  # Used for RecordIO reading.
-        "numpy",
-        "optuna",
-        "pretrainedmodels",
-        "scikit-image",
-        "scikit-learn",
-        "scipy",
-        "torch",
-        "torchvision",
-        "Pillow",
-        "PyYAML",
-        "gitpython",
-        "wandb",
-        "pycocotools"
+        "faiss-cpu==1.7.2",  # Need for MAP@R metric computation.
+        "jpeg4py==0.1.4",
+        "mxnet==2.0.0b1",  # Used for RecordIO reading.
+        "numpy==1.23.4",
+        "optuna==3.0.3",
+        "pretrainedmodels==0.7.4",
+        "scikit-image==0.19.3",
+        "scikit-learn==1.1.3",
+        "scipy==1.8.1",
+        "torch==1.12.1",
+        "torchvision==0.13.1",
+        "Pillow==9.3.0",
+        "PyYAML==6.0",
+        "gitpython==3.1.29",
+        "wandb==0.13.5",
+        "pycocotools==2.0.6"
     ]
 )
